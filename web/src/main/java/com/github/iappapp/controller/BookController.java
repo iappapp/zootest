4f58cbd34b7ac5fb2f4c093fd4df2fe404a6f1d0
yanan.g055 <yanan.g055@shulidata.com>
2020-01-20 16:10:20 +0800

    add web module

---
+++ web/src/main/java/com/github/iappapp/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.github.iappapp.controller;
+
+import com.github.iappapp.modal.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    private Book book;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Object printBook() {
+        return book.toString();
+    }
+}
