68b5567f06b4ba2bdb8b706f7474b81e4fad1ee7
iappapp <iadore@live.cn>
2020-02-11 17:43:16 +0800

    add freemarker template

--- web/src/main/java/com/github/iappapp/controller/BookController.java
+++ web/src/main/java/com/github/iappapp/controller/BookController.java
@@ -1,12 +1,15 @@
 package com.github.iappapp.controller;
 
 import com.github.iappapp.modal.Book;
+import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/book")
@@ -20,8 +23,16 @@ public class BookController {
         return book.toString();
     }
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView returnIndex() {
-        return new ModelAndView("/index");
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ModelAndView returnHello() {
+        Map<String, Object> map = Maps.newHashMap();
+        map.putIfAbsent("userName", "user");
+        map.putIfAbsent("userAge", 13);
+        return new ModelAndView("hello").addAllObjects(map);
     }
 }
